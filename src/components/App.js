5c80910a0e3606c5da08413f0323b578bd461f7d
Chris Y Cho <chrisycho04@gmail.com>
2019-02-22 15:47:19 -0800

    testing with react-transition-group. successful animation slide in/out and fade in/out. appear doesnt work

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,13 @@ import Footer from './Footer';
 import FeaturedStocks from  './FeaturedStocks';
 import { fetchCompanyInfo, fetchFinancialInfo, fetchQuoteInfo, fetchNews, fetchImages, fetchSymbols, fetchChartInfo } from '../actions';
 import './style/App.css';
+import { CSSTransition } from 'react-transition-group';
+import Test from './Test';
 
 class App extends React.Component {
+    //for transition purposes
+    state={ clicked: false }
+
     componentDidMount(){
         const stock = 'AAPL'
         this.props.fetchCompanyInfo(stock);
@@ -20,6 +25,10 @@ class App extends React.Component {
         this.props.fetchImages(10);
         this.props.fetchSymbols(this.props, this.generateRandomNumbers);
         this.props.fetchChartInfo(stock);
+    }
+
+    animation = () => {
+        this.setState({ clicked: !this.state.clicked })
     }
 
     generateRandomNumbers = props => {
@@ -34,6 +43,10 @@ class App extends React.Component {
     render(){
         return(
             <div className='app'>
+                <CSSTransition in={this.state.clicked} classNames='test2' appear timeout={500} unmountOnExit>
+                    <Test />
+                </CSSTransition>
+                <div onClick={this.animation} className={this.state.clicked ? 'false' : 'true'}>Testing</div>
                 <FeaturedStocks />
                 <StockSearch />
                 <div className= 'ui container'>    
