5f5dbe34aed87828c148f0a73e70e64c842d594a
Chris Cho <chrisycho04@gmail.com>
2019-11-02 14:41:35 -0700

    new dependecies. Material ui added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
 import { bounceInUp } from "react-animations";
+import Grid from '@material-ui/core/Grid';
 
 import StockDetails from "./StockDetails";
 import MainDisplay from "./MainDisplay";
@@ -8,16 +9,24 @@ import Footer from "./Footer";
 
 const bounceUpAnimation = keyframes`${bounceInUp}`;
 
-const BodyWrapper = styled.div`
+const BodyWrapper = styled(Grid)`
   animation: 1s ${bounceUpAnimation};
 `;
 
+const LeftGrid = styled(Grid)`
+  background-color: gray;
+  height: 100vh;
+`
+
 const Body = () => {
   return (
-    <BodyWrapper className="ui container">
+    <BodyWrapper container>
+      <LeftGrid item xs={2}></LeftGrid>
+      <Grid item xs={10}>
       <MainDisplay />
       <StockDetails />
       <Footer />
+      </Grid>
     </BodyWrapper>
   );
 };
